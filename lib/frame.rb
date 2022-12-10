bca37bb885349a9ffede384b9ba877a3041143e0
m <32762403+m-codes@users.noreply.github.com>
2022-12-10 20:55:04 +0000

    more testing for frame spec and frame_score added

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,15 +1,16 @@
 class Frame
 
-  attr_accessor :r1, :r2
+  attr_accessor :r1, :r2, :frame_score
 
   def initialize(r1, r2)
-    
+
     if r1 > 10 || r2 > 10 || r1 + r2 > 10
       fail "Invalid input"
     end
 
     @r1 = r1
     @r2 = r2
+    @frame_score = @r1 + @r2
 
   end
 
@@ -20,6 +21,5 @@ class Frame
   def is_spare?
     !is_strike? && @r1 + @r2 == 10 ? true : false
   end
-
 
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -20,7 +20,27 @@ RSpec.describe Frame do
 
   context 'when given more then 10 for any roll' do
     it 'fails' do
-      expect{Frame.new(11,2)}.to raise_error "Invalid input"
+      expect{Frame.new(11,0)}.to raise_error "Invalid input"
+      expect{Frame.new(0,15)}.to raise_error "Invalid input"
+      expect{Frame.new(8,8)}.to raise_error "Invalid input"
+    end
+  end
+
+  context 'when given r1 and r2 as spare' do
+    it 'returns frame score' do
+      frame_1 = Frame.new(6,4)
+      expect(frame_1.frame_score).to eq 10
+      expect(frame_1.is_spare?).to eq true
+      expect(frame_1.is_strike?).to eq false
+    end
+  end
+
+  context 'when given r1 and r2 as open' do
+    it 'returns frame score' do
+      frame_1 = Frame.new(6,2)
+      expect(frame_1.frame_score).to eq 8
+      expect(frame_1.is_spare?).to eq false
+      expect(frame_1.is_strike?).to eq false
     end
   end
 
